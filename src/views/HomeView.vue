49fc352274c48f1be293c8c6383408cf4adad840
luohuidong <luohuidong01@126.com>
2022-11-21 21:22:09 +0800

    refactor: remove useless file

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1 class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+    Hello world!
+  </h1>
 </template>
